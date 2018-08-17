1929d2c6b0daa8552fa4b4efa52f3d8b3b18cf40
Patrick McCombs <pa.mccombs1@gmail.com>
2018-08-17 09:17:02 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,6 +7,6 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
-  
+  # projects: kickstarter.css("li.project.grid_4")
   binding.pry
 end
